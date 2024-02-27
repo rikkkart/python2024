7c6dd198c43835fbf117b8ed4ff0a89955a2f908
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2024-02-27 16:21:55 +0300

    Созданы пакеты для разных зон ответственности

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,6 +1,6 @@
 import pytest
-from contact import Contact
-from application import Application
+from model.contact import Contact
+from fixture.application import Application
 
 @pytest.fixture
 def app(request):
