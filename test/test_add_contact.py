caa4004be38d75f8a42fcebc11b187699fd9c280
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2024-02-29 17:11:43 +0300

    Повышени е интеллектуальности фикстуры, проверка валидации перед каждым тестом

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,7 +4,6 @@ from fixture.application import Application
 
 
 def test_add_contact(app):
-    app.session.login("admin", "secret")
     app.contact.create(Contact("jfvjk", "dfvwfv", "vsdfvw",
                                         "vsfv", "vfvebertb","bebergb",
                                         "fbeb", "beregr", "ebegrber",
@@ -12,14 +11,13 @@ def test_add_contact(app):
                                         "gbtbthh", "hrth", "hrhr", "thth",
                                         "11", "September",
                                         "2000", "12", "June", "2004"))
-    app.session.logout()
+
 
 def test_add_empty_contact(app):
-    app.session.login("admin", "secret")
     app.contact.create(Contact("", "", "", "", "",
                         "", "", "", "", "", "",
                      "", "", "", "", "-", "-",
                     "", "-", "-", ""))
-    app.session.logout()
+
 
 
--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -1,6 +1,5 @@
 
 
 def test_add_contact(app):
-    app.session.login("admin", "secret")
     app.contact.delete_first_contact()
-    app.session.logout()
+
